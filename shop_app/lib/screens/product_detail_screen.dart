89779f8ba2fcca7e9582798804ff3196a81573c7
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2019-12-04 06:59:20 +0100

    Navigating Detail screen using pushNamed(ProductDetailScreen.routeName, arguments: id)

--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/dummy_data.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  final String title;
-  ProductDetailScreen(this.title);
+  // final String title;
+  // ProductDetailScreen(this.title);
 
+  static const routeName = '/product-detail';
   @override
   Widget build(BuildContext context) {
+    final id = ModalRoute.of(context).settings.arguments as String;
+    final product = DummyData.loadedProduct.firstWhere((p)=> p.id == id);
     return Scaffold(
-      appBar: AppBar(title: Text(title),),
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
     );
   }
 }
