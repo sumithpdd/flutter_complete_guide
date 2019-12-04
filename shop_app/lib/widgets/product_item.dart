03034e23e353150b3cb2e6ee63a27b47c2eeefe1
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2019-12-04 06:39:31 +0100

    On the fly route creation not the best way for big apps

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -10,16 +11,24 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-          child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (ctx) => ProductDetailScreen(title),
+            ));
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
             icon: Icon(Icons.favorite),
-            onPressed: () {},color: Theme.of(context).accentColor,
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
           ),
           title: Text(title, textAlign: TextAlign.center),
           trailing: IconButton(
